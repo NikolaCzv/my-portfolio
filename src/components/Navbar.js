ca0a69c47ff31889e13c5c89ed0071bf220a2530
github NikolaCzv <nikola.raicic@hotmail.com>
2020-06-01 21:21:16 -0500

    contact, added library

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,11 @@ import React from "react";
 import prog from "../assets/prog.png";
 import logo from "../assets/logo.jpg";
 import styled from "styled-components";
-import { Divider, Button, Icon } from "semantic-ui-react";
+import {
+    Divider,
+    Button,
+    Icon
+} from "semantic-ui-react";
 
 const Container = styled.div`
     display: flex;
@@ -67,12 +71,19 @@ class Navbar extends React.Component{
                         <Divider hidden/>
                         <h3>I write clean and easy to read code.</h3>
                         <Divider hidden />
-                        <Button color="twitter">
-                            <Icon name="envelope outline"/> Contact
+                        <Button
+                            color="twitter"
+                            href="mailto:nikola.raicic@hotmail.com">
+                            <Icon name="envelope outline"/>
+                            Contact
                         </Button>
-                        <Button color='linkedin' href="https://www.linkedin.com/in/nikola-raicic-9b6424195/">
-                            <Icon name='linkedin' /> LinkedIn
+                        <Button
+                            color='linkedin'
+                            href="https://www.linkedin.com/in/nikola-raicic-9b6424195/">
+                            <Icon name='linkedin' />
+                            LinkedIn
                         </Button>
+                        <Divider hidden />
                     </LeftContainer>
                     <RightContainer>
                         <Image src={prog} />
