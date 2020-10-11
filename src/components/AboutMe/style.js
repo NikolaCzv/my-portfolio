17190c7830cc0b9bc9332b152fbf33f4c7ac3c0c
github NikolaCzv <nikola.raicic@hotmail.com>
2020-10-11 11:59:53 -0500

    about me fix

--- src/components/AboutMe/style.js
+++ src/components/AboutMe/style.js
@@ -13,6 +13,11 @@ export const Container = styled.div`
     background-color: ${colors.white};
     border-radius: 5px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    overflow: scroll;
+
+    ::-webkit-scrollbar {
+      display: none;
+    }
 
     @media (max-width: 768px) {
         width: 100%;
